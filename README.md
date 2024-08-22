df679086b0491edf35ac357e86d1692e6e0593c0
brent-thomas <brent@itsnotabugitsafeature.com>
2024-08-21 22:33:26 -0400

    added enemy and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+Attribution:
+
+Background:
+<a href="https://pixabay.com/photos/stars-sky-night-starry-sky-1845140/">Outer Space Background Image</a>
+
+Game icon:
+<a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by Pixel Buddha - Flaticon</a>
+
+Player icon:
+<a href="https://www.flaticon.com/free-icons/spaceship" title="spaceship icons">Spaceship icons created by Freepik - Flaticon</a>
+
+Enemy icon:
+<a href="https://www.flaticon.com/free-icons/space-invaders" title="space-invaders icons">Space-invaders icons created by Payungkead - Flaticon</a>
--- main.py
+++ main.py
@@ -1,14 +1,74 @@
 import pygame
-
+import random
 #Init pygame
 pygame.init()
 
 #Create the screen
 screen = pygame.display.set_mode((800,600))
 
+#Set title and icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+#load background image
+background = pygame.image.load('space_background.jpg')
+
+# Create a transparent surface
+overlay = pygame.Surface((800, 600))  
+overlay.set_alpha(130)  
+overlay.fill((0, 0, 0))  
+
+
+# Player 
+player_icon = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+playerY_change = 0
+
+def player(x,y):
+    screen.blit(player_icon,(x,y))
+
+# Enemy 
+enemy_icon = pygame.image.load('enemy.png')
+enemyX = random.randint(0,800)
+enemyY = random.randint(50,150)
+enemyX_change = 0
+enemyY_change = 0
+
+def enemy(x,y):
+    screen.blit(enemy_icon,(x,y))
+
+    
+
 #Game loop
 running = True
 while running:
+    #set screen color
+    screen.blit(background, (0, 0)) 
+    screen.blit(overlay, (0, 0))
+   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #if keystoke is pressed check whether it's right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -1
+            if event.key == pygame.K_RIGHT:
+                 playerX_change = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0.0
+
+    # Check if the player has gone off the screen
+    if playerX < -32:
+        playerX = 800
+    elif playerX > 832:
+        playerX = 0
+    
+    playerX += playerX_change
+    player(playerX,playerY)
+    enemy(enemyX,enemyY)
+    pygame.display.update()
